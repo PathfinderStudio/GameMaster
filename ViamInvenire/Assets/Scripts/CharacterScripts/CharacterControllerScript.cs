d5cfc9c9ce6e21f59591d96a8a91eb360304a847
Chandler Waugh <chandler@gbpackerfans.com>
2017-02-02 01:06:32 -0700

    Started character controller.  Needs work for jumping, and air control.Fix that.  Camera rotate player around XZ plane when looking left and right, rotate camera up and down for other rotation.  Neither work perfectly.

---
+++ ViamInvenire/Assets/Scripts/CharacterScripts/CharacterControllerScript.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterControllerScript : MonoBehaviour
+{
+    public double runSpeed { get; set; }
+ 
+
+    private double _runSpeed;
+    private Rigidbody player;
+    private Vector3 playerMovement;
+    private bool jumping;
+    private float jumpAmount;
+    private bool canMove;
+
+    // Use this for initialization
+    void Start()
+    {
+        runSpeed = 100.0;
+        _runSpeed = runSpeed;
+        player = this.GetComponent<Rigidbody>();
+        playerMovement = Vector3.zero;
+        jumping = false;
+        jumpAmount = 500;
+  
+        canMove = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.timeScale != 0)
+        {
+            if (Input.GetKeyDown(KeyCode.LeftShift) || Input.GetKeyDown(KeyCode.RightShift))
+            {
+                _runSpeed = runSpeed * 2.0;
+            }
+            else if (Input.GetKeyUp(KeyCode.LeftShift) || Input.GetKeyUp(KeyCode.RightShift))
+            {
+                _runSpeed = runSpeed;
+            }
+
+            playerMovement.x = Input.GetAxis("Horizontal");
+            playerMovement.z = Input.GetAxis("Vertical");
+            if(canMove)
+            {
+                player.AddForce(playerMovement * (float)_runSpeed);  //prevent movement left to right and forward back, while off the ground, use raycast to find distance to ground?
+            }
+           
+            if (player.velocity.x > 10)
+            {
+                player.velocity = new Vector3(10, player.velocity.y, player.velocity.z);
+            }
+            if (player.velocity.z > 10)
+            {
+                player.velocity = new Vector3(player.velocity.x, player.velocity.y, 10);
+            }
+            if (player.velocity.x < -10)
+            {
+                player.velocity = new Vector3(-10, player.velocity.y, player.velocity.z);
+            }
+            if (player.velocity.z < -10)
+            {
+                player.velocity = new Vector3(player.velocity.x, player.velocity.y, -10);
+            }
+            //player.transform.Translate(playerMovement.x * (float)_runSpeed * Time.deltaTime, 0, playerMovement.z * (float)_runSpeed * Time.deltaTime);
+
+            if (Input.GetKeyDown(KeyCode.Space) && !jumping)
+            {
+                jumping = true;
+                player.AddForce(Vector3.up * jumpAmount);
+            }
+            if (player.velocity.y == 0)
+            {
+                jumping = false;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision col)
+    {
+        if(col.transform.tag == "Ground")
+        {
+            Debug.Log("On Ground");
+            canMove = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision col)
+    {
+        if(col.transform.tag == "Ground")
+        {
+            Debug.Log("Not on ground");
+            canMove = false;
+        }
+    }
+}
---
+++ ViamInvenire/Assets/Scripts/CharacterScripts/MouseAimCamera.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseAimCamera : MonoBehaviour {
+
+    // Use this for initialization
+    // speed is the rate at which the object will rotate
+    public float speed = 5.0f;
+
+    private Vector3 look;
+
+    void FixedUpdate()
+    {
+        look.x = 0; //Input.GetAxis("Mouse X");
+        look.y = Input.GetAxis("Mouse X");
+        Debug.Log(look.y);
+        look.z = 0;
+
+        this.transform.Rotate(look, speed);
+        
+    }
+}
